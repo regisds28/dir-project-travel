582ea56c7525b5aa09002b4dc0ebee5db1a3b79e
regisds28 <regisparaujo@gmail.com>
2020-12-16 16:59:14 -0300

    adicionando regras de mensagens

--- travel.js
+++ travel.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const expressHandlebars = require("express-handlebars");
 const message = require("./lib/message");
+const handlers = require("./lib/handlers");
 
 //usar assim requer o "type":"module" no package.json
 //import express from "express";
@@ -18,25 +19,16 @@ app.engine(
 
 app.set("view engine", "handlebars");
 
-app.get("/", (req, res) => res.render("home"));
-app.get("/about", (req, res) => {
-  res.render("about", { message: message.getMessages });
-});
+app.get("/", (req, res) => handlers.home);
+app.get("/about", (req, res) => handlers.about);
 
 app.use(express.static(__dirname + "/public"));
 
 //página 404 personalizada
-app.use((req, res) => {
-  res.status(404);
-  res.render("404");
-});
+app.use((req, res) => handlers.notFound);
 
 //página 500 personalizada
-app.use((err, req, res, next) => {
-  console.error(err.message);
-  res.status(500);
-  res.render("500");
-});
+app.use((err, req, res, next) => handlers.serverError);
 
 app.listen(port, () =>
   console.log(
